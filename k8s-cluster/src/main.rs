8086b9f4bcce35361be67bf7718e228e5ce99633
greg <greg.cusack@solana.com>
2023-10-18 21:45:52 +0000

    cargo fmt

--- k8s-cluster/src/main.rs
+++ k8s-cluster/src/main.rs
@@ -691,7 +691,7 @@ async fn main() {
             return;
         }
     }
-    
+
     // Begin Kubernetes Setup and Deployment
     let config_map = match kub_controller.create_genesis_config_map().await {
         Ok(config_map) => {
