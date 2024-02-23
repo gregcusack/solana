b12200d2c3d7d21158ac603b530e4bd3eec1f775
greg <greg.cusack@anza.xyz>
2024-02-23 19:40:31 +0000

    fix bug in client script

--- k8s-cluster/src/scripts/client-startup-script.sh
+++ k8s-cluster/src/scripts/client-startup-script.sh
@@ -125,7 +125,7 @@ bench-tps)
       --threads $threadCount \
       $benchTpsExtraArgs \
       --read-client-keys ./client-accounts.yml \
-      --url "http://$LOAD_BALANCER_RPC_ADDRESS"
+      --url "http://$test_rpc"
       ${args[*]} \
       ${runtime_args[*]} \
   "
