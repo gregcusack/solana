915f82d94f6dd4e0dad906983f23b94f1f483044
greg <greg.cusack@anza.xyz>
2024-02-28 22:58:36 +0000

    wip. trying to switch localcluster bench test to use tpuclient

--- bench-tps/tests/bench_tps.rs
+++ bench-tps/tests/bench_tps.rs
@@ -1,8 +1,7 @@
 #![allow(clippy::arithmetic_side_effects)]
 
 use {
-    serial_test::serial,
-    solana_bench_tps::{
+    serial_test::serial, solana_bench_tps::{
         bench::{do_bench_tps, generate_and_fund_keypairs},
         cli::{Config, InstructionPaddingConfig},
         send_batch::generate_durable_nonce_accounts,
@@ -10,25 +9,16 @@ use {
     solana_client::{
         connection_cache::ConnectionCache,
         tpu_client::{TpuClient, TpuClientConfig},
-    },
-    solana_core::validator::ValidatorConfig,
-    solana_faucet::faucet::run_local_faucet,
-    solana_local_cluster::{
-        local_cluster::{ClusterConfig, LocalCluster},
+    }, solana_connection_cache::connection_cache, solana_core::validator::ValidatorConfig, solana_faucet::faucet::run_local_faucet, solana_local_cluster::{
+        local_cluster::{self, ClusterConfig, LocalCluster},
         validator_configs::make_identical_validator_configs,
-    },
-    solana_rpc::rpc::JsonRpcConfig,
-    solana_rpc_client::rpc_client::RpcClient,
-    solana_sdk::{
+    }, solana_rpc::rpc::JsonRpcConfig, solana_rpc_client::rpc_client::RpcClient, solana_sdk::{
         account::{Account, AccountSharedData},
         commitment_config::CommitmentConfig,
         fee_calculator::FeeRateGovernor,
         rent::Rent,
         signature::{Keypair, Signer},
-    },
-    solana_streamer::socket::SocketAddrSpace,
-    solana_test_validator::TestValidatorGenesis,
-    std::{sync::Arc, time::Duration},
+    }, solana_streamer::socket::SocketAddrSpace, solana_test_validator::TestValidatorGenesis, std::{sync::Arc, time::Duration}
 };
 
 fn program_account(program_data: &[u8]) -> AccountSharedData {
@@ -55,9 +45,8 @@ fn test_bench_tps_local_cluster(config: Config) {
     let faucet_addr = run_local_faucet(faucet_keypair, None);
 
     const NUM_NODES: usize = 1;
-    let cluster = LocalCluster::new(
-        &mut ClusterConfig {
-            node_stakes: vec![999_990; NUM_NODES],
+    let mut cluster_config = ClusterConfig {
+        node_stakes: vec![999_990; NUM_NODES],
             cluster_lamports: 200_000_000,
             validator_configs: make_identical_validator_configs(
                 &ValidatorConfig {
@@ -72,7 +61,9 @@ fn test_bench_tps_local_cluster(config: Config) {
             native_instruction_processors,
             additional_accounts,
             ..ClusterConfig::default()
-        },
+    };
+    let cluster = LocalCluster::new(
+        &mut cluster_config,
         SocketAddrSpace::Unspecified,
     );
 
