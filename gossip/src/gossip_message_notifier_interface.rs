a5cc906d8e227ba425ee0eee24f7ccfafcdac657
greg <greg@anza.xyz>
2024-11-01 21:33:53 +0000

    add node removal update notication

--- gossip/src/gossip_message_notifier_interface.rs
+++ gossip/src/gossip_message_notifier_interface.rs
@@ -1,8 +1,11 @@
-use {crate::contact_info::ContactInfo, std::sync::Arc};
+use {crate::contact_info::ContactInfo, solana_sdk::pubkey::Pubkey, std::sync::Arc};
 
 pub trait GossipMessageNotifierInterface: std::fmt::Debug {
     /// Notified when a CrdsValue is upserted
-    fn notify_receive_message(&self, contact_info: &ContactInfo);
+    fn notify_receive_node_update(&self, contact_info: &ContactInfo);
+
+    /// Notified when a node is removed/purged from crds table
+    fn notify_remove_node(&self, pubkey: &Pubkey);
 }
 
 pub type GossipMessageNotifier = Arc<dyn GossipMessageNotifierInterface + Sync + Send>;
